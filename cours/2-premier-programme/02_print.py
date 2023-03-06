667bc1793b22143bb47233e768d80cb07eef8d5c
quent <quentin.bidolet@gmail.com>
2023-03-06 12:08:58 +0100

    - print

---
+++ cours/2-premier-programme/02_print.py
@@ -0,0 +1,34 @@
+# Fonction print
+print("Hello World!")
+
+# Séparateur
+print("Hello", "World", sep="%")
+
+# end
+print("Hello", "World", sep="%", end="$$\n")
+
+# On peut utiliser des guillemets simple
+print('Hello World!')
+
+# mettre une variable
+print("Je suis {0} et {1}.".format("Quentin", "argument 2"))
+
+# niveau 2
+print("Je suis " + "Quentin et " + "argument 2.")
+
+# échappement
+print('J\'ai un escape.')
+
+# Retour à la ligne
+print("Ligne 1\nLigne 2")
+
+# Plus simplement, on peut écrire
+print("""Ligne 1
+Ligne 2
+""")
+
+"""
+Exercice : Afficher le texte suivant.
+5-6$$$
+7
+"""
