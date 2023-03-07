73e4bc9125e269a9be1bf5ebbc5a3a80f0f8df58
quent <quentin.bidolet@gmail.com>
2023-03-07 15:35:15 +0100

    - fonctions.py

--- cours/6-procedures-et-fonctions/01_fonctions.py
+++ cours/6-procedures-et-fonctions/01_fonctions.py
@@ -50,3 +50,51 @@ def somme(*args):
 resultat = somme(1, 2, 3, 5, 6, 7)
 print(resultat)
 
+# fonctions anonymes
+print("#" * 25)
+
+
+def double(a):
+    return a * 2
+
+
+print(double(2))
+liste = [1, 2, 3, 4]
+# nouvelle_liste = []
+# for nombre in liste:
+#     nouvelle_liste.append(double(nombre))
+# print(nouvelle_liste)
+
+mon_map = map(double, liste)
+print(list(mon_map))
+
+mon_map = map(lambda x: x % 2, liste)
+print(list(mon_map))
+
+# filter
+ma_liste = [6, 7, 8, 9]
+nombres_pairs_filter_object = filter(lambda a: a % 2 == 0, ma_liste)
+nombres_pairs = list(nombres_pairs_filter_object)
+
+
+# closure
+def fonction_externe(x):
+    def fonction_interne(y):
+        return x + y
+
+    return fonction_interne
+
+closure = fonction_externe(10)
+print(fonction_externe(5))
+
+print("#"*25)
+
+# fonctions génératrices
+def range(n):
+    compteur = 0
+    while compteur < n:
+        compteur += 1
+        yield compteur
+
+for i in range(5):
+    print(i)
