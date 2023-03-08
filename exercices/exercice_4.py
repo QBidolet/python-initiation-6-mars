45750a840ce8d6f054ca3ed5312274796ecaf196
quent <quentin.bidolet@gmail.com>
2023-03-08 10:28:13 +0100

    - exercice 4

--- exercices/exercice_4.py
+++ exercices/exercice_4.py
@@ -5,3 +5,25 @@ doit nous retourner la liste des chats saisies dans l'ordre de saisie
 Bonus : ne pas pouvoir saisir deux fois le même nom et afficher le numéro du chat à saisir ( example : saisir chat numéro 1 etc ... )
 Votre programme doit gérer les exceptions de saisie
 """
+
+chats = []
+reponse = ""
+mots_stop = ["stop", "exit", "quit", "quitter"]
+while reponse not in mots_stop:
+    reponse = input(f"Entrez un nom de chat n°{len(chats)+1}\n")
+    reponse = reponse.strip().capitalize()
+    if reponse.lower() in mots_stop:
+        break
+    try:
+        if reponse == "":
+            raise ValueError("Attention pas nom vide.")
+    except ValueError as e:
+        print(e)
+    else:
+        if reponse not in chats:
+            chats.append(reponse)
+        else:
+            print(f"Vous avez déjà mis ce nom de chat ({reponse}).")
+
+for index, nom_chat in enumerate(chats):
+    print(f"Chat n°{index + 1} : {nom_chat}")
